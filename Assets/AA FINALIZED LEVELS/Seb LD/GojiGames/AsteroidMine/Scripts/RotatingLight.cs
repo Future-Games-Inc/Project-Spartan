f386d73027b4a2865163c1530edf2756c3f25bfc
NaijaAvenger <mondaeatughonu@gmail.com>
2022-10-10 18:24:53 -0400

    10/10/22 - Initial Push12

---
+++ Assets/AA FINALIZED LEVELS/Seb LD/GojiGames/AsteroidMine/Scripts/RotatingLight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingLight : MonoBehaviour
+{
+    [SerializeField] private Transform rotatingObjectTransform;
+
+    [SerializeField] private float speed = 1.0f;
+
+    void Update()
+    {
+        rotatingObjectTransform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
